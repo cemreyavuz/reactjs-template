a39240f286546e2e0a3c55434160ae027aaba6ef
Cüneyt Emre Yavuz <cyavuz15@ku.edu.tr>
2019-07-23 13:28:31 +0300

    react-intl package is implemented. createBrowserHistory function call is updated.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
 import ReduxStore from 'state/store/ReduxStore';
 import Routes from 'routing/Routes';
+import { IntlProvider } from 'react-intl';
+import localeEN from 'common/i18n/en.json'
 import './index.scss';
 
 export const history = createBrowserHistory({ //eslint-disable-line
   basename: '',
 });
 
+const locale = 'en';
+
+const languages = {
+  en: localeEN,
+};
+
 ReactDOM.render(
-  <Provider store={ReduxStore.store}>
-    <Router history={history}>
-      <Route component={Routes} />
-    </Router>
-  </Provider>,
+  <IntlProvider locale={locale} messages={languages[locale]}>
+    <Provider store={ReduxStore.store}>
+      <Router history={history}>
+        <Route component={Routes} />
+      </Router>
+    </Provider>
+  </IntlProvider>,
   document.getElementById('root'),
 );
--- src/modules/app/App.js
+++ src/modules/app/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
 
 class App extends Component {
   render() {
     return (
       <div>
-        App Component
+        <FormattedMessage id="app.title" />
       </div>
     );
   }
